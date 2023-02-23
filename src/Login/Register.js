590b432c0b58f7e8fd8a1ed10210761b83af49b3
Muslim <79920735+muslim556@users.noreply.github.com>
2023-02-23 12:47:00 +0500

    fix

--- src/Login/Register.js
+++ src/Login/Register.js
@@ -1,40 +1,50 @@
-import React, { memo } from 'react'
-import "./Login.css"
+import React, { memo, useState } from "react";
+import SignIn from "./SignIn";
 
 function Register() {
-    // const [data, setData] = useState([])
+  const [arr, setArr] = useState(
+    JSON.parse(localStorage.getItem("user")) || []
+  );
 
-
-
-    function onFormValue(e) {
-        e.preventDefault()
-      if (e.target.password.value === e.target.ConfirmPassword.value) {
-          const targetData = e.target;
-          const value = {};
-          for (let i = 0; i < targetData.length; i++) {
-            if (targetData[i].name !== "") {
-              value[targetData[i].name] = targetData[i].value;
-            }
-            localStorage.setItem("user", JSON.stringify(value));
-          }
-          return value;
-      } else {
-        console.log("dovdir xato qilding");
-       }
+  const onFormValue = (e) => {
+    e.preventDefault();
+    if (e.target.password.value === e.target.ConfirmPassword.value) {
+      const targetData = e.target;
+      const value = {};
+      for (let i = 0; i < targetData.length; i++) {
+        if (targetData[i].name !== "") {
+          value[targetData[i].name] = targetData[i].value;
+          setArr([...arr, { ...value }]);
+        }
+        localStorage.setItem("user", JSON.stringify([...arr, { ...value }]));
       }
+      return value;
+    } else {
+      alert("dovdir xato qilding");
+    }
+  };
 
   return (
     <>
-    <form className='register' onSubmit={(e) => onFormValue(e)}>
-        <input type="text" name='name' placeholder='Name' />
-        <input type="text" name='email' placeholder='Email'/>
-        <input type="text" name='password' placeholder='Password'/>
-        <input type="text" name='ConfirmPassword' placeholder='Password'/>
-        <input type="submit" value="submit"/>
-    </form>
-    
+      <form className="register" onSubmit={(e) => onFormValue(e)}>
+        <input type="text" name="name" placeholder="Name" />
+        <input type="text" name="email" placeholder="Email" />
+        <input type="text" name="password" placeholder="Password" />
+        <input type="text" name="ConfirmPassword" placeholder="Password" />
+        <input type="submit" value="submit" />
+      </form>
+      {arr.map((item, i) => {
+        return (
+          <div key={i}>
+            <p>{item.name}</p>
+            <p>{item.email}</p>
+            <p>{item.ConfirmPassword}</p>
+          </div>
+        );
+      })}
+      <SignIn data={arr} />
     </>
-  )
+  );
 }
 
-export default memo(Register)
+export default memo(Register);
---
+++ src/Login/SignIn.js
@@ -0,0 +1,28 @@
+import React, { memo } from "react";
+
+function SignIn({ data }) {
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const filtered = data.filter(
+            (item) => item.name === e.target.name.value
+          );
+
+          if (filtered[0].ConfirmPassword === e.target.signinPassword.value) {
+            console.log(filtered[0].ConfirmPassword);
+          } else {
+            console.log("xato");
+          }
+        }}
+      >
+        <input type="text" name="name" />
+        <input type="text" name="signinPassword" />
+        <input type="submit" value="submit" />
+      </form>
+    </>
+  );
+}
+
+export default memo(SignIn);
